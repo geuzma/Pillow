860f62a0f47f0e41e00d6e7e95256336dbd10d60
barbotp <paul.barbot@gmail.com>
2013-07-17 16:04:44 +0200

    clean test

--- Tests/test_big_frombuffer.py
+++ Tests/test_big_frombuffer.py
@@ -1,40 +1,15 @@
-import sys
-import os
-
-if "--installed" in sys.argv:
-    sys_path_0 = sys.path[0]
-    del sys.path[0]
+from tester import *
 
 from PIL import Image
-
-if "--installed" in sys.argv:
-    sys.path.insert(0, sys_path_0)
-ROOT = '.'
-
-
-print("-"*68)
-print("Python modules loaded from", os.path.dirname(Image.__file__))
-print("Binary modules loaded from", os.path.dirname(Image.core.__file__))
-print("-"*68)
-
-#im = Image.open(os.path.join(ROOT, "Images/lena.jpg"))
-#im.load()
-#size = int(sys.argv[2]) 
-size = 15000
-byte_size  = size * size  * 3 * 4
-print "byte_size: ", byte_size
-print "int_max:   ", 2**32 / 2
-print "byte_size > int_max: ", byte_size > 2**32 / 2
-
-
-#im = Image.new("RGB", (size, size))
-#print im
-
+#FIXME remove dependency
 import numpy as np
-buff = np.random.randint(0, 256, (size, size, 3))
 
+# create a big image > 2gb in memory
+size = 15000
+buff = np.random.randint(0, 256, (size, size, 3))
 im = Image.frombuffer('RGB', (size, size), buff, 'raw', 'RGB', 0, 1)
-if im == None:
-    print "frombuffer failed"
-else:
-    print im
+
+assert (im.size == (size, size))
+#assert len(im.tobytes()) == 1300
+
+print('ok')
